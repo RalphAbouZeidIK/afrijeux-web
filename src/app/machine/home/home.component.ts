97a587794d1e46a5322499fcc654969c281c45fd
RalphabouzeidIK <rabouzeid@intersektion.com>
2026-05-14 10:36:39 +0300

    banner-update

--- src/app/machine/home/home.component.ts
+++ src/app/machine/home/home.component.ts
@@ -9,11 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent {
 
   carouselImages = [
-    { id: '1', url: 'assets/images/banner1.png', alt: 'Banner 1' },
-    { id: '2', url: 'assets/images/banner2.png', alt: 'Banner 2' },
-    { id: '3', url: 'assets/images/banner3.png', alt: 'Banner 3' },
-    { id: '4', url: 'assets/images/banner4.png', alt: 'Banner 4' },
-    { id: '5', url: 'assets/images/banner5.png', alt: 'Banner 5' }
+    { id: '1', url: 'assets/images/banner1-new.png', alt: 'Banner 1' },
+    { id: '2', url: 'assets/images/banner2-new.png', alt: 'Banner 2' },
+    { id: '3', url: 'assets/images/banner3-new.png', alt: 'Banner 3' },
+    { id: '4', url: 'assets/images/banner4-new.png', alt: 'Banner 4' },
+    { id: '5', url: 'assets/images/banner5-new.png', alt: 'Banner 5' }
   ];
 
   constructor() {
