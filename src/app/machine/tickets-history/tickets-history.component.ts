02a7a6bf1adb030efad05aa6facc8c7cfb4805ee
RalphAbouZeidIK <rabouzeid@intersektion.com>
2026-07-06 14:48:58 +0300

    tickets-history fixes

--- src/app/machine/tickets-history/tickets-history.component.ts
+++ src/app/machine/tickets-history/tickets-history.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MachineService } from 'src/app/services/machine.service';
 
 interface TicketHistoryItem {
@@ -13,13 +14,14 @@ interface TicketHistoryItem {
 
 @Component({
   selector: 'app-tickets-history',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './tickets-history.component.html',
   styleUrl: './tickets-history.component.scss'
 })
 export class TicketsHistoryComponent implements OnInit {
   tickets: TicketHistoryItem[] = [];
   isLoading = true;
+  searchTicketId = '';
 
   constructor(private machineSrv: MachineService) { }
 
@@ -55,6 +57,18 @@ export class TicketsHistoryComponent implements OnInit {
 
   get totalTickets(): number {
     return this.tickets.length;
+  }
+
+  onSearchTicketIdChange(value: string) {
+    this.searchTicketId = value.replace(/\D/g, '').slice(0, 24);
+  }
+
+  get filteredTickets(): TicketHistoryItem[] {
+    const search = this.searchTicketId;
+    if (!search) {
+      return this.tickets;
+    }
+    return this.tickets.filter(ticket => ticket.FullTicketId?.toLowerCase().includes(search));
   }
 
   get totalStake(): number {
