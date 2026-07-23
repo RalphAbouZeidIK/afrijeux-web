fe27b237ef04c2461fbb0139b0d75045663ccc05
RalphAbouZeidIK <rabouzeid@intersektion.com>
2026-07-23 11:50:10 +0300

    fixes

--- src/app/machine/machine-route.ts
+++ src/app/machine/machine-route.ts
@@ -81,8 +81,8 @@ export const machineMenuRoutes: Route[] = [
         component: TicketsHistoryComponent,
         data: {
             breadcrumb: 'Tickets History',
-            shouldBeLoggedIn: false,
-            showLink: false,
+            shouldBeLoggedIn: true,
+            showLink: true,
             title: 'Tickets History',
             iconLink: 'assets/images/tickets-history.svg'
         }
